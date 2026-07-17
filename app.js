6388993ca1dafac82fa164856556e539f0820d33
aamir <aamirkhan10136@gmail.com>
2026-07-17 04:05:46 -0700

    making this readable

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-// add new file in main branch (testing how the branch works)
-//button
-//form
+// This file was added on the main branch to test how Git branches work.
+// This placeholder identifies where future button-related JavaScript can go.
+// This placeholder identifies where future form-related JavaScript can go.
